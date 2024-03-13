413918de5404802f5160f1d195eef9d2c09213fc
oumadv33 <alomkkalumq@gmail.com>
2024-03-13 14:53:17 -0700

    13-count_shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- ce script affiche tous les genres depuis hbtn_0d_tvshows
+-- afficher le nombre de donner linker lun a lautre.
+
+-- afficherles donnees
+SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
